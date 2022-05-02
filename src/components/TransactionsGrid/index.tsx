b516f89a69003d5dcbd4ff34f4cf595c25c5d01a
Wes <wal2.designer@gmail.com>
2022-05-02 20:57:48 +0100

    try005

--- src/components/TransactionsGrid/index.tsx
+++ src/components/TransactionsGrid/index.tsx
@@ -7,39 +7,40 @@ export function TransactionsGrid() {
     if (transactions.length <= 0) {
         return <>none</>
     } else {
-        return (
-            <Container>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Amount</th>
-                            <th>Category</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
+        return <>None</>
+        // return (
+        //     <Container>
+        //         <table>
+        //             <thead>
+        //                 <tr>
+        //                     <th>Title</th>
+        //                     <th>Amount</th>
+        //                     <th>Category</th>
+        //                     <th>Date</th>
+        //                 </tr>
+        //             </thead>
 
-                    <tbody>
-                        {transactions.map((transaction) => (
-                            <tr key={transaction.id}>
-                                <td>{transaction.title}</td>
-                                <td className={transaction.type}>
-                                    {new Intl.NumberFormat('pt-pt', {
-                                        style: 'currency',
-                                        currency: 'EUR',
-                                    }).format(transaction.amount)}
-                                </td>
-                                <td>{transaction.category}</td>
-                                <td>
-                                    {new Intl.DateTimeFormat().format(
-                                        new Date(transaction.createdAt)
-                                    )}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </Container>
-        )
+        //             <tbody>
+        //                 {transactions.map((transaction) => (
+        //                     <tr key={transaction.id}>
+        //                         <td>{transaction.title}</td>
+        //                         <td className={transaction.type}>
+        //                             {new Intl.NumberFormat('pt-pt', {
+        //                                 style: 'currency',
+        //                                 currency: 'EUR',
+        //                             }).format(transaction.amount)}
+        //                         </td>
+        //                         <td>{transaction.category}</td>
+        //                         <td>
+        //                             {new Intl.DateTimeFormat().format(
+        //                                 new Date(transaction.createdAt)
+        //                             )}
+        //                         </td>
+        //                     </tr>
+        //                 ))}
+        //             </tbody>
+        //         </table>
+        //     </Container>
+        // )
     }
 }
