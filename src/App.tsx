bd710c3ff9f2ee2c0aee573f90241cf8629e21b9
Wes <wal2.designer@gmail.com>
2022-05-02 19:34:09 +0100

    adding php json

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,48 @@ import { useState } from 'react'
 import { NewTransactionModal } from './components/NewTransactionModal'
 import { TrasactionsProvider } from './hooks/useTransactions'
 
-createServer({
-    models: {
-        transaction: Model,
-    },
+// createServer({
+//     models: {
+//         transaction: Model,
+//     },
 
-    seeds(server) {
-        server.db.loadData({
-            transactions: [
-                {
-                    id: 1,
-                    title: 'Transaction 1',
-                    amount: 400,
-                    type: 'deposit',
-                    category: 'Food',
-                    createdAt: new Date(),
-                },
-                {
-                    id: 2,
-                    title: 'Transaction 2',
-                    amount: 400,
-                    type: 'withdraw',
-                    category: 'Food',
-                    createdAt: new Date(),
-                },
-            ],
-        })
-    },
+//     seeds(server) {
+//         server.db.loadData({
+//             transactions: [
+//                 {
+//                     id: 1,
+//                     title: 'Transaction 1',
+//                     amount: 400,
+//                     type: 'deposit',
+//                     category: 'Food',
+//                     createdAt: new Date(),
+//                 },
+//                 {
+//                     id: 2,
+//                     title: 'Transaction 2',
+//                     amount: 400,
+//                     type: 'withdraw',
+//                     category: 'Food',
+//                     createdAt: new Date(),
+//                 },
+//             ],
+//         })
+//     },
 
-    routes() {
-        this.namespace = 'api'
+//     routes() {
+//         this.namespace = 'api'
 
-        this.get('/transactions', () => {
-            return this.schema.all('transaction')
-        })
+//         this.get('/transactions', () => {
+//             return this.schema.all('transaction')
+//         })
 
-        this.post('/transactions', (schema, request) => {
-            const data = JSON.parse(request.requestBody)
+//         this.post('/transactions', (schema, request) => {
+//             const data = JSON.parse(request.requestBody)
 
-            return schema.create('transaction', data)
-        })
-    },
-})
+//             return schema.create('transaction', data)
+//         })
+//     },
+// })
 
 export function App() {
     const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
